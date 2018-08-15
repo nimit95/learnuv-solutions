82bbad0a88e843e7c530123c2529c16e081cfd63
Nimit <nimitagg95@gmail.com>
2018-08-15 12:50:24 +0530

    06 fs all async
    Signed-off-by: Nimit <nimitagg95@gmail.com>

--- src/06_fs_allasync.c
+++ src/06_fs_allasync.c
@@ -21,21 +21,25 @@ void open_cb(uv_fs_t* open_req) {
   context_t* context = open_req->data;
 
   /* 3. Create buffer and initialize it */
-
+  char* buf = malloc(sizeof(char) * (BUF_SIZE + 1));
+  uv_buf_t iov = uv_buf_init(buf, BUF_SIZE);
+  context->iov = iov;
   /* 4. Setup read request */
-  uv_fs_t *read_req = NULL; /* = malloc ... */
-  context->read_req = NULL; /* = ? */
-  read_req->data = NULL;    /* = ? */
+  uv_fs_t *read_req = malloc(sizeof(uv_fs_t)); /* = malloc ... */
+  context->read_req = read_req; /* = ? */
+  read_req->data = context;    /* = ? */
 
   /* 5. Read from the file into the buffer */
+  r = uv_fs_read(uv_default_loop(), read_req, open_req->result, &iov, 1, -1, read_cb);
   if (r < 0) CHECK(r, "uv_fs_read");
 }
 
 void read_cb(uv_fs_t* read_req) {
-  int r = 0;;
+  int r = 0;
   if (read_req->result < 0) CHECK(read_req->result, "uv_fs_read callback");
 
   context_t* context = NULL; /* = ? */
+  context = read_req->data; 
 
   /* 6. Report the contents of the buffer */
   log_report("%s", context->iov.base);
@@ -44,10 +48,12 @@ void read_cb(uv_fs_t* read_req) {
   free(context->iov.base);
 
   /* 7. Setup close request */
-  uv_fs_t *close_req = NULL;
+  uv_fs_t *close_req = malloc(sizeof(uv_fs_t));
   /* ? */
 
   /* 8. Close the file descriptor */
+  close_req->data = context;
+  r = uv_fs_close(uv_default_loop(), close_req, context->open_req->result, close_cb);
   if (r < 0) CHECK(r, "uv_fs_close");
 }
 
@@ -55,6 +61,7 @@ void close_cb(uv_fs_t* close_req) {
   if (close_req->result < 0) CHECK(close_req->result, "uv_fs_close callback");
 
   context_t* context = NULL;
+  context = close_req->data;
 
   /* 9. Cleanup all requests and context */
   uv_fs_req_cleanup(context->open_req);
@@ -74,6 +81,7 @@ void init(uv_loop_t *loop) {
   open_req->data = context;
 
   /* 2. Open file */
+  r = uv_fs_open(loop, open_req, filename, O_RDONLY, S_IRUSR, open_cb);
   if (r < 0) CHECK(r, "uv_fs_open");
 }
 
